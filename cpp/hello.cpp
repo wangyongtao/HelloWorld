4c268f7ddb24204861941e31fe28b356f6947b8c
WYT <wangtom365@qq.com>
2020-12-04 16:29:01 +0800

    Create hello.cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// 使用 std 命名空间
+using namespace std;
+
+int main()
+{
+    // cout 用来输出字符串（c + out）
+    // std::endl 是一个操纵符,作用是结束当前行 等于 "\n"
+    cout << "cpp: Hello, world!" << endl;
+    
+    return 0;
+}
